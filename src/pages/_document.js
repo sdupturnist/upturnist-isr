24017614f8ddc11d4d3ff00a47d22a232ee90eb8
iamjaseer <42886352+iamjaseer@users.noreply.github.com>
2024-08-27 07:59:05 -0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,30 +3,32 @@ import Script from "next/script";
 import { Partytown } from '@builder.io/partytown/react';
 
 export default function Document() {
-
-
   const GTM_ID = 'GTM-PGFRFK3X'; 
-
-
 
   return (
     <Html lang="en">
-         <Head>  
-          {/* GTM Script */}
-          <Script 
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${GTM_ID}');
-              `,
-            }}
-          />
+      <Head>  
+        {/* GTM Script */}
+        <Script 
+          id="gtm-script"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),
+                    dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');
+            `,
+          }}
+        />
 
-
-<script
+        <Script
+          id="partytown-config"
           data-partytown-config
           dangerouslySetInnerHTML={{
             __html: `
@@ -37,19 +39,17 @@ export default function Document() {
             `,
           }}
         />
-
-        
-        </Head>
+      </Head>
       <body>
-          {/* GTM noscript fallback */}
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            />
-          </noscript>
+        {/* GTM noscript fallback */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Main />
         <NextScript />
       </body>
