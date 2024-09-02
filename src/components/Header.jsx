f0897925e90a6d2b5ea604153bb3da63953e63c5
iamjaseer <42886352+iamjaseer@users.noreply.github.com>
2024-09-02 06:50:59 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,12 +19,6 @@ function Nav({ initialData, type }) {
   const { setModalFor, setShowModal } = useModalContext()
 
 
-
-
-
-
-
-
   const { dataHeader } = HeaderData(initialData);
 
   const { dataContact } = ContactData(initialData);
@@ -32,29 +26,15 @@ function Nav({ initialData, type }) {
 
 
   //THEME
-  // const [lightTheme, setLightTheme] = useState(true);
 
-  // useEffect(() => {
-  // 	const theme = localStorage.getItem('theme');
-  // 	if (theme === 'light') {
-  // 		setLightTheme(true);
-  // 	} else {
-  // 		setLightTheme(false);
-  // 	}
-  // }, []);
-
-  // useEffect(() => {
-  // 	if (lightTheme) {
-  // 		document.documentElement.classList.add('light');
-  // 		localStorage.setItem('theme', 'light');
-  // 	} else {
-  // 		document.documentElement.classList.remove('light');
-  // 		localStorage.setItem('theme', 'dark');
-  // 	}
-  // }, [lightTheme]);
+  const { isDarkMode, theme, toggleTheme } = useThemeContext();
 
 
-  const { theme, toggleTheme } = useThemeContext();
+
+
+
+
+
 
 
   const jsonString = JSON.stringify(dataHeader);
@@ -153,8 +133,13 @@ function Nav({ initialData, type }) {
             {/* {theme} */}
             <Logo url={`${frontendUrl}images/upturnist-logo.webp`} alt="branding_and_digital_marketing_dubai_upturnist_logo" logoTitle="branding and digital marketing dubai upturnist logo" />
             <div className='wrpr--nav-1'>
-              {/* <button className='hamburger-button cursor-pointer'  onClick={() => setLightTheme(!lightTheme)}> */}
-              {/* <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000000" viewBox="0 0 256 256"><path d="M120,40V16a8,8,0,0,1,16,0V40a8,8,0,0,1-16,0Zm8,24a64,64,0,1,0,64,64A64.07,64.07,0,0,0,128,64ZM58.34,69.66A8,8,0,0,0,69.66,58.34l-16-16A8,8,0,0,0,42.34,53.66Zm0,116.68-16,16a8,8,0,0,0,11.32,11.32l16-16a8,8,0,0,0-11.32-11.32ZM192,72a8,8,0,0,0,5.66-2.34l16-16a8,8,0,0,0-11.32-11.32l-16,16A8,8,0,0,0,192,72Zm5.66,114.34a8,8,0,0,0-11.32,11.32l16,16a8,8,0,0,0,11.32-11.32ZM48,128a8,8,0,0,0-8-8H16a8,8,0,0,0,0,16H40A8,8,0,0,0,48,128Zm80,80a8,8,0,0,0-8,8v24a8,8,0,0,0,16,0V216A8,8,0,0,0,128,208Zm112-88H216a8,8,0,0,0,0,16h24a8,8,0,0,0,0-16Z"></path></svg> */}
+            {/* <button className='hamburger-button cursor-pointer'  onClick={toggleTheme}> */}
+            {/* {theme !== 'light' ? */}
+             {/* <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#000000" viewBox="0 0 256 256"><path d="M235.54,150.21a104.84,104.84,0,0,1-37,52.91A104,104,0,0,1,32,120,103.09,103.09,0,0,1,52.88,57.48a104.84,104.84,0,0,1,52.91-37,8,8,0,0,1,10,10,88.08,88.08,0,0,0,109.8,109.8,8,8,0,0,1,10,10Z"></path></svg> */}
+            {/* : */}
+            {/* <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000000" viewBox="0 0 256 256"><path d="M120,40V16a8,8,0,0,1,16,0V40a8,8,0,0,1-16,0Zm8,24a64,64,0,1,0,64,64A64.07,64.07,0,0,0,128,64ZM58.34,69.66A8,8,0,0,0,69.66,58.34l-16-16A8,8,0,0,0,42.34,53.66Zm0,116.68-16,16a8,8,0,0,0,11.32,11.32l16-16a8,8,0,0,0-11.32-11.32ZM192,72a8,8,0,0,0,5.66-2.34l16-16a8,8,0,0,0-11.32-11.32l-16,16A8,8,0,0,0,192,72Zm5.66,114.34a8,8,0,0,0-11.32,11.32l16,16a8,8,0,0,0,11.32-11.32ZM48,128a8,8,0,0,0-8-8H16a8,8,0,0,0,0,16H40A8,8,0,0,0,48,128Zm80,80a8,8,0,0,0-8,8v24a8,8,0,0,0,16,0V216A8,8,0,0,0,128,208Zm112-88H216a8,8,0,0,0,0,16h24a8,8,0,0,0,0-16Z"></path></svg> */}
+{/*  */}
+            {/* } */}
               {/* </button> */}
               {type == 'normal' ?
                 <>
@@ -185,7 +170,7 @@ function Nav({ initialData, type }) {
     {/* HEADER END */}
     {/* MOBILE MENU */}
     <div className={`header-nav-wrpr  ${isOpen ? "show-nav" : "hidden-nav"}`}>
-      <div className="wrpr bg-primary-custom ">
+      <div className="wrpr bg-sky-950">
 
 
         <button title="Close Menu" aria-label="Close Menu" className="closeButton mb-10" onClick={() => setOpen(!isOpen)}>
@@ -210,7 +195,7 @@ function Nav({ initialData, type }) {
                       <li><Link title="Logo Design link header" onClick={() => setOpen(!isOpen)} aria-label="Logo Design" href="/logo-design/" >Logo Design</Link></li>
                       <li><Link title="Brand Collateral link header" onClick={() => setOpen(!isOpen)} aria-label="Brand Collateral" href="/brand-collateral/" >Brand Collateral</Link></li>
                       <li><Link title="Brand Consulting link header" onClick={() => setOpen(!isOpen)} aria-label="Brand Consulting" href="/branding-consulting/" >Brand Consulting</Link></li>
-                      <li><Link title="Website Design & Development link header" onClick={() => setOpen(!isOpen)} aria-label="Website Design & Development" href="/web-design-agency-dubai/" >Website Design & Development</Link></li>
+                      <li><Link title="Website Design & Development link header" onClick={() => setOpen(!isOpen)} aria-label="Website Design & Development" href="/website-design-uae/" >Website Design & Development</Link></li>
                       <li><Link title="e-Commerce Website link header" onClick={() => setOpen(!isOpen)} aria-label="e-Commerce Website" href="/e-commerce-websites-development/" >e-Commerce Website</Link></li>
                       <li><Link title="App Development link header" onClick={() => setOpen(!isOpen)} aria-label="App Development" href="/app-development/" >App Development</Link></li>
                     </ul>
