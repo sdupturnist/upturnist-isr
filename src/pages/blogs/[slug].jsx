46c715a367e56fe52c20258303a958b6298d9eb1
iamjaseer <42886352+iamjaseer@users.noreply.github.com>
2024-09-06 01:30:42 -0300

    Update [slug].jsx

--- src/pages/blogs/[slug].jsx
+++ src/pages/blogs/[slug].jsx
@@ -1,4 +1,4 @@
-import { frontendUrl } from "@/utils/variables";
+import { frontendUrl, wordpressGraphQlApiUrl } from "@/utils/variables";
 import Layout from "@/components/Layout";
 import { AOSInit } from '@/components/Aos';
 import BlurAnimation from '@/components/BlurAnimation';
@@ -10,8 +10,13 @@ import { useEffect } from "react";
 import { useRouter } from "next/router";
 const { htmlToText } = require('html-to-text');
 
+
 export default function BlogSingle({ singleBLogsData, blogSinglePageData, getAllBlogsData }) {
+
   const router = useRouter();
+
+
+
   const singleBlog = singleBLogsData?.data?.allBlogs?.nodes[0] ?? null;
   const allBlogs = getAllBlogsData?.data?.allBlogs?.nodes ?? null;
 
@@ -30,81 +35,84 @@ export default function BlogSingle({ singleBLogsData, blogSinglePageData, getAll
     });
   };
 
-  useEffect(() => {
-    if (!singleBlog) {
-      router.push('/404');
-    }
-  }, [singleBlog, router]);
+  
 
-  if (!singleBlog) {
-    // Optionally, you can show a loading spinner or some placeholder content here
-    return null;
-  }
+    useEffect(() => {
+    if(!singleBlog) {
+        router.push('/404');
+      }
+      
+    }, [singleBlog])
+    
+
 
   return (
     <>
-      <MetatagsBlogSingle data={[singleBLogsData]} />
-      <Layout>
-        <AOSInit />
-        <section className="blog-single-">
-          <div className="container">
-            <div className="inner">
-              <div className="inner-2">
-                <h1 data-aos="fade-up">{singleBlog.title}</h1>
-                <div data-aos="fade-up">
-                  <Images
-                    imageurl={singleBlog.featuredImage?.node?.sourceUrl || 'sample-link'}
-                    styles={''}
-                    quality={100}
-                    width={'1000'}
-                    height={'500'}
-                    alt={singleBlog.featuredImage?.node?.altText || 'no alt'}
-                    title={singleBlog.featuredImage?.node?.altText || 'no alt'}
-                    placeholder={true}
-                    classes={'w-full block'}
-                  />
-                </div>
-                <div data-aos="fade-up" className="blog-content" dangerouslySetInnerHTML={{ __html: singleBlog.content }} />
-                <p className="text-[24px]">{formatBlogDate(singleBlog.date)}</p>
-                <div className="inner-3">
-                  <h3 data-aos="fade-up">More blogs</h3>
-                  <div className="inner-4">
-                    {allBlogs && allBlogs.filter(post => post.slug !== singleBlog.slug).map((blog, key) => (
-                      <Link
-                        key={key}
-                        title={`Read blog: ${blog.title}`}
-                        aria-label={`Read blog: ${blog.title}`}
-                        href={`${frontendUrl}/blogs/${blog.slug}/`}
-                        data-aos="fade-up"
-                        className="grid gap-5 block"
-                      >
-                        {blog.featuredImage && <Images
-                          imageurl={blog.featuredImage.node.sourceUrl}
-                          styles={''}
-                          quality={100}
-                          width={'500'}
-                          height={'500'}
-                          alt={blog.featuredImage.node.altText}
-                          title={blog.featuredImage.node.altText}
-                          placeholder={false}
-                          classes={'rounded-3xl w-full block transform duration-500 ease-in-out filter grayscale opacity-50 hover:opacity-80 hover:grayscale-0'}
-                        />}
-                        <h2>{blog.title}</h2>
-                        <p><TruncatedText text={cleanHTML(blog.content)} maxLength={400} /></p>
-                      </Link>
-                    ))}
+      {singleBlog && (
+        <>
+          <MetatagsBlogSingle data={[singleBLogsData]} />
+          <Layout>
+            <AOSInit />
+            <section className="blog-single-">
+              <div className="container">
+                <div className="inner">
+                  <div className="inner-2">
+                    <h1 data-aos="fade-up">{singleBlog.title}​</h1>
+                    <div data-aos="fade-up">
+                      <Images
+                        imageurl={singleBlog.featuredImage?.node?.sourceUrl || 'sample-link'}
+                        styles={''}
+                        quality={100}
+                        width={'1000'}
+                        height={'500'}
+                        alt={singleBlog.featuredImage?.node?.altText || 'no alt'}
+                        title={singleBlog.featuredImage?.node?.altText || 'no alt'}
+                        placeholder={true}
+                        classes={'w-full block'}
+                      />
+                    </div>
+                    <div data-aos="fade-up" className="blog-content" dangerouslySetInnerHTML={{ __html: singleBlog.content }} />
+                    <p className="text-[24px]">{formatBlogDate(singleBlog.date)}</p>
+                    <div className="inner-3">
+                      <h3 data-aos="fade-up">More blogs</h3>
+                      <div className="inner-4">
+                        {allBlogs && allBlogs.filter(post => post.slug !== singleBlog.slug).map((blog, key) => (
+                          <Link
+                            key={key}
+                            title={`Read blog: ${blog.title}`}
+                            aria-label={`Read blog: ${blog.title}`}
+                            href={`${frontendUrl}/blogs/${blog.slug}/`}
+                            data-aos="fade-up"
+                            className="grid gap-5 block"
+                          >
+                            {blog.featuredImage && <Images
+                              imageurl={blog.featuredImage.node.sourceUrl}
+                              styles={''}
+                              quality={100}
+                              width={'500'}
+                              height={'500'}
+                              alt={blog.featuredImage.node.altText}
+                              title={blog.featuredImage.node.altText}
+                              placeholder={false}
+                              classes={'rounded-3xl w-full block transform duration-500 ease-in-out filter grayscale opacity-50 hover:opacity-80 hover:grayscale-0'}
+                            />}
+                            <h2>{blog.title}</h2>
+                            <p><TruncatedText text={cleanHTML(blog.content)} maxLength={400} /></p>
+                          </Link>
+                        ))}
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-          <BlurAnimation position="top right" />
-        </section>
-      </Layout>
+              <BlurAnimation position="top right" />
+            </section>
+          </Layout>
+        </>
+      )}
     </>
   );
 }
-
 
 
 
