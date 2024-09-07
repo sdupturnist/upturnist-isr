4204c10acc0f33ffff68808e2a846be68a21e969
iamjaseer <42886352+iamjaseer@users.noreply.github.com>
2024-09-07 08:19:25 -0300

    updated

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,24 +1,22 @@
-// pages/_app.js
 import NoInternetConnection from "@/components/NoConnection";
 import "../../public/styles/globals.min.css";
 //import "../../public/styles/tempstyle.css";
 import CusrsorAnimation from "@/components/CusrsorAnimation";
 import { ModalContextProvider } from "@/context/modalContext";
 import { ThemeProvider } from "@/context/themeContext";
-import { GoogleTagManager } from '@next/third-parties/google';
+
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GoogleTagManager gtmId="GTM-PGFRFK3X" /> {/* Add this line */}
       <NoInternetConnection>
-        <ThemeProvider>
-          <ModalContextProvider>
-            <CusrsorAnimation />
+      <ThemeProvider>
+        <ModalContextProvider>
+          <CusrsorAnimation />
             <Component {...pageProps} />
-          </ModalContextProvider>
+        </ModalContextProvider>
         </ThemeProvider>
       </NoInternetConnection>
     </>
-  );
+  )
 }
